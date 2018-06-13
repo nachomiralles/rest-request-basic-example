31dd0422613c81f371e75096b10dc22c3352b66a
Nacho Miralles <nachomiralles@gmail.com>
2018-06-13 15:13:10 +0200

    First commit

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area  {
+
+    public string nombre;
+    public double latitud;
+    public double longitud;
+    public double radio;
+
+    public Area(string n, double la, double lo, double rad)
+    {
+        this.nombre = n;
+        this.latitud = la;
+        this.longitud = lo;
+        this.radio = rad;
+    }
+
+    public override string ToString()
+    {
+        return "Nombre: " + this.nombre + "\nLatitud: " + this.latitud + "; Longitud: " + this.longitud + "\nRadio: " + this.radio;
+    }
+
+}
---
+++ Assets/Scripts/PedirDatos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class PedirDatos : MonoBehaviour {
+
+    string URL = "https://firestore.googleapis.com/v1beta1/projects/tfg-kiko-web/databases/(default)/documents/Areas";
+    ArrayList areas = new ArrayList();
+    Text textoPantalla;
+
+    IEnumerator Start()
+    {
+        using (WWW www = new WWW(URL))
+        {
+            yield return www;
+            var content = JSON.Parse(www.text);
+            for(int i = 0; i<content["documents"].Count; i++)
+            {
+                string name = content["documents"][i]["fields"]["nombre"]["stringValue"];
+                double lat = content["documents"][i]["fields"]["latitud"]["doubleValue"];
+                double lon = content["documents"][i]["fields"]["longitud"]["doubleValue"];
+                double rad = content["documents"][i]["fields"]["radio"]["doubleValue"];
+                Area a = new Area(name, lat, lon, rad);
+                areas.Add(a);
+            }
+            setUIText();
+        }
+    }
+
+    private void setUIText()
+    {
+        textoPantalla = GetComponent<Text>();
+        string texto = "";
+        
+        foreach(Area a in areas)
+        {
+            texto += a + "\n\n";
+        }
+
+        textoPantalla.text = texto;
+    }
+
+    
+}
